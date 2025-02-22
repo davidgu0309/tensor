e6d1ae9a829f1bd561dc3b99886125645e7a2dc0
Electron1997 <mirco.paul.1997@gmail.com>
2025-02-23 00:46:00 +0100

    Move test tensors into separate file

--- test/functional_unit_tests.cpp
+++ test/functional_unit_tests.cpp
@@ -1,5 +1,6 @@
 #include "../include/functional.hpp"
 #include "../include/tensor.hpp"
+#include "test_tensors.hpp"
 
 #include <iostream>
 #include <numeric>
@@ -89,26 +90,6 @@ int getEntrySafe(const Tensor<int>& t, const MultiIndex& i){
 }
 
 void functionalUnitTests() {
-    
-    // Define tensors for tests
-    Tensor<int> zeros_3x4x5 = zeros<int>({3, 4, 5});
-    Tensor<int> ones_5 = ones<int>({5});
-    Tensor<int> twos_5 = constant<int>({5}, 2);
-    Tensor<int> ones_10 = ones<int>({10});
-    Tensor<int> ones_3x3 = ones<int>({3, 3});
-    Tensor<int> ones_3x2x4 = ones<int>({3, 2, 4});
-    Tensor<int> t1 = Tensor<int>({3, 2, 4}, std::vector<int>(24, -1));
-    Tensor<int> t2 = Tensor<int>({10}, std::vector<int>(10, -1));
-    Tensor<int> t3 = Tensor<int>({3, 3}, std::vector<int>(9, -1));
-    Tensor<int> threes_3x3 = constant<int>({3, 3}, 3);
-    Tensor<int> iota_5 = iota<int>({5});
-    Tensor<int> scalar_10 = Tensor<int>(10);
-    Tensor<int> scalar_15 = Tensor<int>(15);
-    Tensor<int> scalar_55 = Tensor<int>(55);
-    Tensor<int> iota_3x3 = iota<int>({3,3});
-    Tensor<int> iota_2x2x3 = iota<int>({2,2,3});
-    Tensor<int> iota_3x3_squared = Tensor<int>({3,3}, std::vector<int>({30,36,42,66,81,96,102,126,150}));
-    Tensor<int> iota_3D_times_2D_result = Tensor<int>({2,2,3}, std::vector<int>{30,36,42,66,81,96,102,126,150,138,171,204});
     
     std::vector<int> data(24);
     std::iota(data.begin(), data.end(), 1);
